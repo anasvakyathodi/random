ae5570c0e6b2a293257aefb50272f261c27f1954
anasvakyathodi <avtvld@gmail.com>
2020-03-15 15:18:43 +0530

    second edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 function ran_generator() {
-    var limit = document.getElementById("text-input").value;
-    var result = Math.floor((Math.random() * limit) + 1);
-    document.getElementById("content").innerHTML=result;
-
+    if ((document.getElementById("text-input").value) <= 0) {
+      alert("Input a valid number");
+      document.getElementById("content").innerHTML=0;
+    }
+    else {
+      var limit = document.getElementById("text-input").value;
+      var result = Math.floor((Math.random() * limit) + 1);
+      document.getElementById("content").innerHTML=result;
+    }
+}
+function clr_all() {
+  document.getElementById("content").innerHTML=0;
+  document.getElementById("text-input").innerHTML.value="";
 }
